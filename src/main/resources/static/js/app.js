c2f495f07ccb460501e6ee832c7a274fefe5571c
Renato Müller Rosa <renatogordooo@gmail.com>
2019-03-14 22:19:49 -0300

    Criado função para criar o slug

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -62,6 +62,7 @@ const AddDeal = { template: `
           v-model="deal.title"
           :counter="150"
           label="Título"
+          v-on:keyup="slugify"
           required>
         </v-text-field>
       </v-flex>
@@ -294,7 +295,31 @@ const AddDeal = { template: `
       console.log(formObj);
       axios.post('/api/deal/saveDeal', formObj).then(response => {
         console.log(response);
-    })
+      })
+    },
+    sanitizeTitle: function(title) {
+      var slug = "";
+      // Change to lower case
+      var titleLower = title.toLowerCase();
+      // Letter "e"
+      slug = titleLower.replace(/e|é|è|ẽ|ẻ|ẹ|ê|ế|ề|ễ|ể|ệ/gi, 'e');
+      // Letter "a"
+      slug = slug.replace(/a|á|à|ã|ả|ạ|ă|ắ|ằ|ẵ|ẳ|ặ|â|ấ|ầ|ẫ|ẩ|ậ/gi, 'a');
+      // Letter "o"
+      slug = slug.replace(/o|ó|ò|õ|ỏ|ọ|ô|ố|ồ|ỗ|ổ|ộ|ơ|ớ|ờ|ỡ|ở|ợ/gi, 'o');
+      // Letter "u"
+      slug = slug.replace(/u|ú|ù|ũ|ủ|ụ|ư|ứ|ừ|ữ|ử|ự/gi, 'u');
+      // Letter "d"
+      slug = slug.replace(/đ/gi, 'd');
+      // Trim the last whitespace
+      slug = slug.replace(/\s*$/g, '');
+      // Change whitespace to "-"
+      slug = slug.replace(/\s+/g, '-');
+
+      return slug;
+    },
+    slugify: function() {
+      this.deal.url = this.sanitizeTitle(this.deal.title);
     }
   },
   mounted () {}
